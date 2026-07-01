91059900a49c90fe402725eb5281ee1c475e2b2e
Poorvik Vernekar <poorvik@example.com>
2026-07-01 22:55:43 +0530

    Serve media files on Railway

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
+from django.urls import re_path
 from users import views as user_views
 
 urlpatterns = [
@@ -57,7 +58,12 @@ path(
 
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {
+            'document_root': settings.MEDIA_ROOT,
+        },
+    ),
+]
